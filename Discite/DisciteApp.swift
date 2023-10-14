7ae26b4edb5cfbd5170c8ff83e64091b2de87ba8
Jessie Li <jessie.li.24@dartmouth.edu>
2023-10-14 15:44:23 -0400

    Initial Commit

---
+++ Discite/DisciteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisciteApp.swift
+//  Discite
+//
+//  Created by Jessie Li on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DisciteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
