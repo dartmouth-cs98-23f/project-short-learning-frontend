ecf345972b50bbeba2c292d680796cd35ed98cf9
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2024-02-22 19:31:49 -0500

    rebase

--- Discite/Classes/Explore/ViewModels/SearchViewModel.swift
+++ Discite/Classes/Explore/ViewModels/SearchViewModel.swift
@@ -12,11 +12,43 @@ class SearchViewModel: ObservableObject {
     @Published var isFocused = false
     @Published var searchHistory: [String] = ["Internet", "Algorithms"]
     @Published var shouldNavigate = false
+<<<<<<< HEAD
     @Published var searchables: [Searchable] = []
     
     // get search suggestions based on current text
     func getSuggestions(for text: String) -> [Searchable] {
         let predefinedSuggestions = searchables
+=======
+    @StateObject var exploreViewModel = ExploreViewModel()
+    
+    // combine topics and recommendations
+    func createSearchables() -> [Searchable] {
+        var searchables: [Searchable] = []
+        
+        // add topic recommendations
+        for topic in exploreViewModel.topicRecommendations {
+            let searchable = Searchable(id: topic.id.uuidString, name: topic.topicName, type: .topic, topic: topic, playlist: nil)
+            searchables.append(searchable)
+            print("topic loop")
+        }
+        
+        // add playlist recommendations
+        if let playlists = exploreViewModel.playlistRecommendations {
+            for playlist in playlists {
+                let searchable = Searchable(id: playlist.id.uuidString, name: playlist.title, type: .playlist, topic: nil, playlist: playlist)
+                searchables.append(searchable)
+            }
+        }
+
+        print("Searchables: \(searchables)")
+        
+        return searchables
+    }
+    
+    // get search suggestions based on current text
+    func getSuggestions(for text: String) -> [Searchable] {
+        let predefinedSuggestions = createSearchables()
+>>>>>>> fc0d644 (rebase)
         let filteredSuggestions = predefinedSuggestions.filter { $0.name.lowercased().contains(text.lowercased()) }
 
         return filteredSuggestions
@@ -27,6 +59,7 @@ class SearchViewModel: ObservableObject {
         // Set shouldNavigate to true to trigger navigation
         shouldNavigate = true
     }
+<<<<<<< HEAD
 
     // *** displaying search-related stuff below ***
     func showSearchHistory() -> some View {
@@ -49,10 +82,39 @@ class SearchViewModel: ObservableObject {
                     })
                 }
             }
+=======
+    
+    func DestinationView() -> some View {
+        Text("Search Text: \(searchText)")
+    }
+
+    // *** displaying search-related stuff below***
+    func showSearchHistory() -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Search History")
+                .font(Font.caption)
+                .foregroundColor(.gray)
+                .padding(.leading, 16)
+            
+            Divider()
+
+            ForEach(searchHistory.reversed(), id: \.self) { searchItem in
+                NavigationLink(destination: {
+                    SearchDestinationView(searchText: searchItem)
+                }, label: {
+                    Text(searchItem)
+                    .font(Font.body)
+                    .padding(.horizontal, 16)
+                })
+            }
+
+            Spacer()
+>>>>>>> fc0d644 (rebase)
         }
     }
     
     func showSuggestions() -> some View {
+<<<<<<< HEAD
         ScrollView(.vertical) {
             VStack(alignment: .leading, spacing: 8) {
                 Text("Suggestions")
@@ -74,6 +136,29 @@ class SearchViewModel: ObservableObject {
                     })
                 }
             }
+=======
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Suggestions")
+                .font(Font.caption)
+                .foregroundColor(.gray)
+                .padding(.leading, 16)
+                .frame(alignment: .leading)
+            
+            Divider()
+
+            ForEach(self.getSuggestions(for: searchText), id: \.id) { suggestion in
+                NavigationLink(destination: {
+                    //
+                }, label: {
+                    Text(suggestion.name)
+                        .font(Font.body)
+                        .padding(.horizontal, 16)
+                        .frame(alignment: .leading)
+                })
+            }
+
+            Spacer()
+>>>>>>> fc0d644 (rebase)
         }
     }
 }
