cbff353e204e300cbc301553ee8b0a2446b7958a
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2024-02-22 19:31:49 -0500

    rebase

--- Discite/Classes/Explore/ViewModels/SearchViewModel.swift
+++ Discite/Classes/Explore/ViewModels/SearchViewModel.swift
@@ -40,7 +40,7 @@ class SearchViewModel: ObservableObject {
     
     // get search suggestions based on current text
     func getSuggestions(for text: String) -> [Searchable] {
-        let predefinedSuggestions = createSearchables()
+        let predefinedSuggestions = searchables
         let filteredSuggestions = predefinedSuggestions.filter { $0.name.lowercased().contains(text.lowercased()) }
 
         return filteredSuggestions
@@ -51,6 +51,7 @@ class SearchViewModel: ObservableObject {
         // Set shouldNavigate to true to trigger navigation
         shouldNavigate = true
     }
+<<<<<<< HEAD
 
     // *** displaying search-related stuff below ***
     func showSearchHistory() -> some View {
@@ -73,10 +74,39 @@ class SearchViewModel: ObservableObject {
                     })
                 }
             }
+=======
+    
+    func DestinationView() -> some View {
+        Text("Search Text: \(searchText)")
+    }
+
+    // *** displaying search-related stuff below***
+    func showSearchHistory() -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Search History")
+                .font(Font.caption)
+                .foregroundColor(.gray)
+                .padding(.leading, 16)
+            
+            Divider()
+
+            ForEach(searchHistory.reversed(), id: \.self) { searchItem in
+                NavigationLink(destination: {
+                    SearchDestinationView(searchText: searchItem)
+                }, label: {
+                    Text(searchItem)
+                    .font(Font.body)
+                    .padding(.horizontal, 16)
+                })
+            }
+
+            Spacer()
+>>>>>>> fc0d644 (rebase)
         }
     }
     
     func showSuggestions() -> some View {
+<<<<<<< HEAD
         ScrollView(.vertical) {
             VStack(alignment: .leading, spacing: 8) {
                 Text("Suggestions")
@@ -98,6 +128,29 @@ class SearchViewModel: ObservableObject {
                     })
                 }
             }
+=======
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Suggestions")
+                .font(Font.caption)
+                .foregroundColor(.gray)
+                .padding(.leading, 16)
+                .frame(alignment: .leading)
+            
+            Divider()
+
+            ForEach(self.getSuggestions(for: searchText), id: \.id) { suggestion in
+                NavigationLink(destination: {
+                    //
+                }, label: {
+                    Text(suggestion.name)
+                        .font(Font.body)
+                        .padding(.horizontal, 16)
+                        .frame(alignment: .leading)
+                })
+            }
+
+            Spacer()
+>>>>>>> fc0d644 (rebase)
         }
     }
 }
