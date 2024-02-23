a1bd8d5db69193ef218260135ed6305d390a05a0
jessieli24 <113321943+jessieli24@users.noreply.github.com>
2024-02-23 10:27:56 -0500

    Saved Page (#34)
    * working on saved page
    
    * custom tab view
    
    * fixed custom tab view
    
    * cleaned up and smooth animation
    
    * fixed topic link color
    
    * moved custom tab view to common folder
    
    * filter out unsaved topics
    
    ---------
    
    Co-authored-by: Jessie Li <jessieli@Jessies-MacBook.local>

---
+++ Discite/Classes/Account/Personal/SavedViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  SavedViewModel.swift
+//  Discite
+//
+//  Created by Jessie Li on 2/19/24.
+//
+
+import Foundation
+
+class SavedViewModel: ObservableObject {
+    @Published var error: Error?
+    @Published var savedPlaylists: [PlaylistPreview] = []
+    @Published var savedTopics: [TopicTag] = []
+    
+    init() { }
+    
+    // GET saved playlists and topics
+    public func mockGetSaved() async {
+        error = nil
+        
+        do {
+            print("TEST: GET saved")
+            let response = try await APIRequest<EmptyRequest, SavedResponse>
+                .mockRequest(method: .get,
+                            authorized: false,
+                            path: "/api/saved")
+            
+            savedPlaylists = response.savedPlaylists
+            savedTopics = response.savedTopics
+            
+        } catch {
+            print("Error in SavedViewModel.getSaved: \(error)")
+            self.error = error
+        }
+    }
+    
+    // Filters unsaved topics out of savedTopics
+    public func filterSavedTopics() {
+        let filteredTopics = savedTopics.filter { topic in
+            return topic.isSaved
+        }
+        
+        savedTopics = filteredTopics
+    }
+}
+
+struct SavedResponse: Codable {
+    var savedPlaylists: [PlaylistPreview]
+    var savedTopics: [TopicTag]
+}
