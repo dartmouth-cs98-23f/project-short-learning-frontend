fd7266da0f4b74b598cbc1a27ee41b51bd53c306
Jessie Li <jessieli@Jessies-MacBook.local>
2024-03-09 11:41:10 -0500

    working save/unsave

---
+++ Discite/Classes/Saved/Watch/SinglePlaylistWatchCover.swift
@@ -0,0 +1,40 @@
+//
+//  SinglePlaylistWatchCover.swift
+//  Discite
+//
+//  Created by Jessie Li on 3/9/24.
+//
+
+import SwiftUI
+
+struct SinglePlaylistWatchCover: View {
+    @Environment(\.dismiss) var dismiss
+    let playlistId: String
+    
+    var body: some View {
+        WatchSinglePlaylist(playlistId: playlistId)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+    
+                } label: {
+                    HStack(alignment: .center, spacing: 4) {
+                        Image(systemName: "chevron.left")
+                        Text("Back")
+                    }
+                    .font(.button)
+                    .foregroundStyle(Color.secondaryPeachLight)
+                }
+                .foregroundStyle(Color.secondaryPeachLight)
+            }
+        }
+    }
+}
+
+#Preview {
+    SinglePlaylistWatchCover(playlistId: "65d8fc3495f306b28d1b88d6")
+        .environment(TabSelectionManager.init(selection: .Explore))
+        .environmentObject(User())
+}
